1c4fa84e10e7b70164392be8edb0e63abe46ced3
Adonay Veiga <adonayveiga@gmail.com>
2020-11-27 17:35:09 -0300

    All working

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,33 @@
 class RestaurantsController < ApplicationController
+  before_action :fetch_restaurant, only: %i[edit update show destory]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(create_restaurant_params)
+    if @restaurant.save
+      flash[:success] = 'Restaurant successfully created'
+      redirect_to @restaurant
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
+  private
+
+  def fetch_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def create_restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+  before_action :fetch_restaurant
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(create_review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      flash[:success] = 'Review successfully created'
+      redirect_to restaurant_path(@restaurant)
+    else
+      flash[:error] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
+  private
+
+  def fetch_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def create_review_params
+    params.require(:review).permit(:rating, :content)
+  end
 end
